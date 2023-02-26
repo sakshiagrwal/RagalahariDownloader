47c75becc278d0b33d91cf7b0ee68e501128e8ad
Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-02-26 08:04:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+"""
+Ragalahari Downloader
+"""
+
+import os
+from urllib.parse import urljoin
+from concurrent.futures import ThreadPoolExecutor
 import requests
 from bs4 import BeautifulSoup
-import os
 
+# Input URL
 url = input("Enter the url of the page you want to download images from: ")
 
 # Create a session object
@@ -10,8 +17,8 @@ session = requests.Session()
 # Make a request to the URL using the session object
 response = session.get(url)
 
-# Parse the HTML content using lxml parser
-soup = BeautifulSoup(response.content, "lxml")
+# Parse the HTML content using html5lib parser
+soup = BeautifulSoup(response.content, "html5lib")
 
 # Find all the links that point to images
 image_links = []
@@ -19,45 +26,67 @@ for a in soup.find_all("a"):
     if a.find("img"):
         href = a.get("href")
         if href.endswith(".aspx"):
-            image_links.append("https://www.ragalahari.com" + href)
+            image_links.append(urljoin(url, href))
 
 # Create a folder to store the images
-folder_name = url.split("/")[-1].replace(".aspx", "")
+folder_name = os.path.basename(url).replace(".aspx", "")
 if not os.path.exists(folder_name):
     os.makedirs(folder_name)
 
-# Download all the images
-for img_url in image_links:
+# Download all the images using a thread pool
+def download_image(img_url):
+    """Download an image from a URL and save it to a folder.
+
+    Args:
+        img_url (Any): The URL of the image to download.
+
+    Returns:
+        None: This function doesn't return anything, but it saves the downloaded image to a folder.
+
+    Raises:
+        Exception: If there's an error downloading or saving the image.
+    """
     try:
         # Get the filename of the image
-        filename = img_url.split("/")[-1].replace(".aspx", ".jpg")
+        filename = os.path.basename(img_url).replace(".aspx", ".jpg")
 
-        # Download the image
-        response = session.get(img_url)
+        # Check if the image has already been downloaded
+        filepath = os.path.join(folder_name, filename)
+        if os.path.exists(filepath):
+            print(f"{filename} already exists")
+            return
 
-        # Parse the HTML content to get the actual image URL
-        soup = BeautifulSoup(response.content, "lxml")
-        img_tag = soup.find("img", {"data-srcset": True})
-        if img_tag:
-            actual_img_url = (
-                img_tag["data-srcset"]
-                .split(",")[-1]
-                .split(" ")[0]
-                .replace(
-                    "https://szcdn.ragalahari.com", "https://starzone.ragalahari.com"
+        # Check if the folder name is in the image URL
+        if folder_name in img_url:
+            # Download the image
+            img_response = session.get(img_url)
+
+            # Parse the HTML content to get the actual image URL
+            img_soup = BeautifulSoup(img_response.content, "html.parser")
+            img_tag = img_soup.find("img", {"data-srcset": True})
+            if img_tag:
+                actual_img_url = (
+                    img_tag["data-srcset"]
+                    .split(",")[-1]
+                    .split(" ")[0]
+                    .replace(
+                        "https://szcdn.ragalahari.com",
+                        "https://starzone.ragalahari.com",
+                    )
                 )
-            )
-            # Print the actual image URL for debugging
-            print(actual_img_url)
 
-            # Save the image to the folder
-            filepath = os.path.join(folder_name, filename)
-            with open(filepath, "wb") as handler:
-                handler.write(session.get(actual_img_url).content)
+                # Save the image to the folder
+                with open(filepath, "wb") as handler:
+                    handler.write(session.get(actual_img_url).content)
 
-            print(f"Downloaded {filename}")
+                print(f"Downloaded {filename} from '{actual_img_url}'")
+            else:
+                print(f"No image found at {img_url}")
         else:
-            print(f"No image found at {img_url}")
-            print(soup)
-    except Exception as e:
-        print(f"Error downloading {img_url}: {e}")
+            return
+    except requests.exceptions.RequestException as err:
+        print(f"Error downloading {img_url}: {err}")
+
+
+with ThreadPoolExecutor(max_workers=8) as executor:
+    executor.map(download_image, image_links)
