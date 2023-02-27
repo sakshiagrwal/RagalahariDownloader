a13e4c97b8c9a180699579d1c728586bdfbea76c
Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-02-27 19:13:47 +0530

    Revert2
    Signed-off-by: Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-"""
-Ragalahari Downloader
-"""
-
 import os
 import os.path
 import shutil
@@ -11,33 +7,11 @@ import requests
 
 
 def exists(site, path):
-    """
-    Check if a file exists at a given URL path.
-
-    Args:
-        site (str): The base URL of the site.
-        path (str): The path to the file.
-
-    Returns:
-        bool: True if the file exists, False otherwise.
-    """
     r = requests.head(site + path)
     return r.status_code == requests.codes.ok
 
 
 def check_file_exists(file_path, file_name, cycle, id_lists):
-    """
-    Check if a series of files exists at a given URL path.
-
-    Args:
-        file_path (str): The base URL of the site.
-        file_name (str): The name of the files to check for.
-        cycle (int): The number of files to check.
-        id_lists (list): A list to store the IDs of the files that exist.
-
-    Returns:
-        None
-    """
     i = 1
     print("Searching files...")
     while i < cycle:
@@ -52,31 +26,11 @@ def check_file_exists(file_path, file_name, cycle, id_lists):
 
 
 def create_folder(folder_name):
-    """
-    Create a new folder with the given name, if it does not already exist.
-
-    Args:
-        folder_name (str): The name of the folder to create.
-
-    Returns:
-        None
-    """
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
 
 
 def download_images(file_path, folder_name, id_lists):
-    """
-    Download images from the specified URL path and save them in a folder with the given name.
-
-    Args:
-        file_path (str): The URL path where the images are located.
-        folder_name (str): The name of the folder to save the images in.
-        id_lists (list): A list of image IDs to download.
-
-    Returns:
-        None
-    """
     os.chdir(folder_name)
     for x in id_lists:
         r = requests.get(file_path + x, stream=True)
@@ -88,18 +42,6 @@ def download_images(file_path, folder_name, id_lists):
 
 
 def main():
-    """
-    Main function for downloading images from a specified URL path.
-
-    This function prompts the user for the URL path, file name, folder name, and number of images
-    to download, and then downloads the images and saves them in a folder with the specified name.
-
-    Args:
-        None
-
-    Returns:
-        Nothing
-    """
     try:
         # Getting user inputs
         file_path = input("Enter the URL path of the images: ")
