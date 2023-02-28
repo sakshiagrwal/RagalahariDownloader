e66eff4ba97de06595cc4c5bea71e9311490cab9
Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-02-28 19:27:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""
+Ragalahari Downloader
+"""
+
 import os
 import os.path
 import shutil
@@ -5,7 +9,12 @@ import sys
 import requests
 
 
-def check_file_exists(site_url: str, file_name_format: str, num_images: int, id_lists: list) -> bool:
+def check_file_exists(
+    site_url: str, file_name_format: str, num_images: int, id_lists: list
+) -> bool:
+    """
+    Checks if the specified images exist on the server and returns a list of their IDs.
+    """
     print("\033[93mSearching for files...\033[0m")
 
     found_files = True
@@ -32,6 +41,9 @@ def check_file_exists(site_url: str, file_name_format: str, num_images: int, id_
 
 
 def download_images(site_url: str, folder_name: str, id_lists: list) -> None:
+    """
+    Downloads the specified images to the specified folder.
+    """
     os.chdir(folder_name)
 
     print("\033[93mDownloading images...\033[0m")
@@ -52,22 +64,36 @@ def download_images(site_url: str, folder_name: str, id_lists: list) -> None:
 
 
 def main() -> None:
+    """
+    Main function to handle user input and download the images.
+    """
     try:
         # Getting user inputs
         site_url = input("\033[94mEnter the URL path of the images: \033[0m")
-        num_images = int(input("\033[94mHow many images do you want to download? Enter 0 to download all. (Default: 10): \033[0m") or 10)
-        file_name_format = input("\033[94mEnter the file name format (e.g. image-%d.jpg): \033[0m")
+        num_images = int(
+            input(
+                "\033[94mHow many images do you want to download? (Default: 10): \033[0m"
+            )
+            or 10
+        )
+        file_name_format = input(
+            "\033[94mEnter the file name format (e.g. image-%d.jpg): \033[0m"
+        )
 
         # Re-ask the user until valid file names are entered
         while True:
             if "%d" in file_name_format:
                 id_lists = []
-                if num_images == 0 or check_file_exists(site_url, file_name_format, num_images, id_lists):
+                if check_file_exists(site_url, file_name_format, num_images, id_lists):
                     break
                 else:
-                    file_name_format = input("\033[91mEnter a valid file name format:\033[0m ")
+                    file_name_format = input(
+                        "\033[91mEnter a valid file name format:\033[0m "
+                    )
             else:
-                file_name_format = input("\033[91mInvalid file name format. Please include '%d' in the format:\033[0m ")
+                file_name_format = input(
+                    "\033[91mInvalid file name format. Please include '%d' in the format:\033[0m "
+                )
 
         # Prompt for folder name
         folder_name = input("\033[94mEnter the folder name: \033[0m")
@@ -77,22 +103,14 @@ def main() -> None:
             os.makedirs(folder_name)
 
         # Download the images
-        if num_images == 0:
-            id_lists = []
-            check_file_exists(site_url, file_name_format, sys.maxsize, id_lists)
-            download_images(site_url, folder_name, id_lists)
-        else:
-            id_lists = []
-            if check_file_exists(site_url, file_name_format, num_images, id_lists):
-                download_images(site_url, folder_name, id_lists[:num_images])
-            else:
-                print("\033[91mCould not download any files\033[0m")
+        download_images(site_url, folder_name, id_lists[:num_images])
 
     except KeyboardInterrupt:
         print("\n\033[91mShutdown requested. Exiting...\033[0m")
     except requests.exceptions.Timeout:
         print("\033[91mRequest timed out. Exiting...\033[0m")
     sys.exit(1)
+
 
 if __name__ == "__main__":
     main()
