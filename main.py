a43d365c2dffe54ce273f863ca53e59d7922af57
Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-03-05 07:05:47 +0530

    fix

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ from urllib.parse import urlparse
 full_url = input("Enter the full URL of the first image: ")
 
 parsed_url = urlparse(full_url)
-site_url = f"{parsed_url.scheme}://{parsed_url.netloc}{parsed_url.path}"
+site_url = f"{parsed_url.scheme}://{parsed_url.netloc}{os.path.dirname(parsed_url.path)}/"
 folder_name = os.path.splitext(os.path.basename(parsed_url.path))[0]
-file_name_format = os.path.splitext(parsed_url.path)[0] + "%d" + os.path.splitext(parsed_url.path)[1]
+file_name = os.path.splitext(os.path.basename(parsed_url.path))[0]
 
 num_images = 4
 if not os.path.exists(folder_name):
     os.makedirs(folder_name)
 
 for i in range(1, num_images + 1):
-    file_url = site_url + file_name_format % i
+    file_name_format = file_name + str(i) + os.path.splitext(parsed_url.path)[1]
+    file_url = site_url + file_name_format
     response = requests.get(file_url)
     if response.status_code == 200:
-        file_name = file_name_format % i
-        with open(os.path.join(folder_name, file_name), "wb") as file:
+        with open(os.path.join(folder_name, file_name_format), "wb") as file:
             file.write(response.content)
-            print(f"{file_name} - Downloaded successfully!")
+            print(f"{file_name_format} - Downloaded successfully!")
     else:
-        print(f"{file_name_format % i} not found on server")
+        print(f"{file_name_format} not found on server")
